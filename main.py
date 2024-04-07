ebec96238ac57754f7100016997a0dd8e8e97d63
BrhamaKumar <146445615+BrhamaKumar@users.noreply.github.com>
2024-04-07 19:31:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ logging = logging.getLogger()
 
 
 bot = Client("bot",
-             bot_token=os.environ.get("BOT_TOKEN", "6737613081:AAEIRA_f_oo9IQ5DUwVybbUHF4rrzI9slrU"),
+             bot_token=os.environ.get("BOT_TOKEN", "6316340060:AAFmWUEd2sMzNxyaRGvXQMX55NM6---cD1c"),
              api_id=int(os.environ.get("API_ID", "20810825")),
              api_hash=os.environ.get("API_HASH", "707e67f53b4593a3e9b6b424311f84d0"))
 auth_users = [6301693754]
